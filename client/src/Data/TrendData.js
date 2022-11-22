bb6804cb0dd3e6c969b0b186d024b82d7d2fbcaa
iamsauvik01 <sauvikdas64@gmail.com>
2022-11-22 21:27:18 +0530

    First
    test 1

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "wintermeals",
+    shares: 97,
+  },
+  {
+    name: "foodrangers",
+    shares: 80.5,
+  },
+  {
+    name: "iamfoodie",
+    shares: 75.5,
+  },
+  {
+    name: "chicken",
+    shares: 72,
+  },
+  {
+    name: "hungryhunk",
+    shares: 71.9,
+  },
+  {
+    name: "foodieiam",
+    shares: 20,
+  },
+];
